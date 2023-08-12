7a26660a4229387c7e71def54ddb807a8cd58d0e
BUR073 <93581465+BUR073@users.noreply.github.com>
2023-08-12 20:36:59 +0100

    Update RTGQueueSystem.py
    Introduces a GUI with tkinter

--- RTGQueueSystem.py
+++ RTGQueueSystem.py
@@ -1,106 +1,110 @@
-# List to store RTGs currently on tour
+import tkinter as tk
+from tabulate import tabulate
+
 onTour = []
+tout = ["HEB", "ABC", "EFG", "GHI", "GHF", "IUY", "YRT", "NMB"]
+timeOut = ["YUT"]
 
-# Lists to manage RTGs in different states
-tout = ["HEB", "ABC", "EFG", "GHI", "GHF", "IUY", "YRT", "NMB"]  # RTGs available for tour
-timeOut = ["YUT"]  # RTGs in timeout
-
-# Function to add an RTG to appropriate list
 def addRTG(name):
-    name = name.strip()  # Remove whitespace
+    name = name.strip()
     if len(tout) < 8:
-        # Add RTG to tout if there are less than 8 RTGs on tour
         tout.append(name)
     else:
-        # Add RTG to timeout if tout is full
         timeOut.append(name)
 
-# Function to remove an RTG from appropriate list
 def removeRTG(name):
     if name in tout:
-        tout.remove(name)  # Remove from tout list
+        tout.remove(name)
     elif name in timeOut:
-        timeOut.remove(name)  # Remove from timeout list
+        timeOut.remove(name)
     elif name in onTour:
-        print("You cannot remove RTG from roster when on tour.")  # Cannot remove while on tour
+        print("You cannot remove RTG from roster when on tour.")
 
-# Function to send an RTG on tour
 def sendOnTour(name):
     if name in tout:
         if name == tout[0]:
-            onTour.append([name])  # Add RTG to onTour list
-            tout.remove(name)  # Remove from tout list
+            onTour.append([name])
+            tout.remove(name)
             
         if timeOut:
-            tout.append(timeOut[0])  # Add next person from timeout to tout queue
-            del timeOut[0]  # Remove from timeout
+            tout.append(timeOut[0])
+            del timeOut[0]
     else:
         print("Name is not valid")
 
-# Function to return an RTG from tour
 def returnFromTour(name):
-    name = name.strip()  # Trim whitespace
-    found = False  # Initialize a flag to track if the name is found
+    name = name.strip()
+    found = False
     
-    # Iterate through onTour list to find the RTG
     for i in onTour:
         if i[0] == name:
-            onTour.remove(i)  # Remove from onTour list
-            addRTG(name)  # Add back using addRTG function
+            onTour.remove(i)
+            addRTG(name)
             found = True
-            break  # Exit loop once found
+            break
     
     if not found:
         print(f"{name} is not on tour")
-
-# Function to display the current state of RTGs
-from tabulate import tabulate
 
 def displayQueue():
     tout_table = [[num, rtg] for num, rtg in enumerate(tout, start=1)]
     timeOut_table = [[num, rtg] for num, rtg in enumerate(timeOut, start=1)]
     onTour_table = [[num, rtg[0]] for num, rtg in enumerate(onTour, start=1)]
     
-    print("\nTouting Queue")
-    print(tabulate(tout_table, headers=["#", "RTG"]))
+    queue_text = f"\nTouting Queue:\n{tabulate(tout_table, headers=['#', 'RTG'])}\n"
+    queue_text += f"\nTimeout Queue:\n{tabulate(timeOut_table, headers=['#', 'RTG'])}\n"
+    queue_text += f"\nOn Tour:\n{tabulate(onTour_table, headers=['#', 'RTG'])}\n"
     
-    print("\nTimeout Queue")
-    print(tabulate(timeOut_table, headers=["#", "RTG"]))
-    
-    print("\nOn Tour")
-    print(tabulate(onTour_table, headers=["#", "RTG"]))
-    print("----------------------\n")
+    queue_label.config(text=queue_text)
 
+def add_rtg_button_clicked():
+    rtg_name = add_rtg_entry.get()
+    addRTG(rtg_name)
+    displayQueue()
 
+def send_on_tour_button_clicked():
+    rtg_name = send_on_tour_entry.get()
+    sendOnTour(rtg_name)
+    displayQueue()
 
-# Main program loop
-def main():
-    while True:
-        print("1. Add RTG")
-        print("2. Remove RTG")
-        print("3. Send RTG on tour")
-        print("4. RTG return from tour")
-        print("5. Display queue")
-        print("6. Exit")
-        x = int(input("Input: "))  # Get user choice
-        print("")
-        
-        # Execute the chosen action
-        if x == 6:
-            break
-        elif x == 1:
-            addRTG(input("RTG name: "))  # Add RTG
-        elif x == 2:
-            removeRTG(input("RTG name: "))  # Remove RTG
-        elif x == 3:
-            sendOnTour(input("RTG name: "))  # Send RTG on tour
-        elif x == 4:
-            returnFromTour(input("RTG name: "))  # Return RTG from tour
-        elif x == 5:
-            displayQueue()  # Display the queue
-        elif x == 6:
-            break
-      
-main()  # Start the main program loop
-    
-  
+def return_from_tour_button_clicked():
+    rtg_name = return_from_tour_entry.get()
+    returnFromTour(rtg_name)
+    displayQueue()
+
+root = tk.Tk()
+root.title("RTG Management System")
+
+add_rtg_label = tk.Label(root, text="Add RTG:")
+add_rtg_label.pack()
+
+add_rtg_entry = tk.Entry(root)
+add_rtg_entry.pack()
+
+add_rtg_button = tk.Button(root, text="Add RTG", command=add_rtg_button_clicked)
+add_rtg_button.pack()
+
+send_on_tour_label = tk.Label(root, text="Send RTG on Tour:")
+send_on_tour_label.pack()
+
+send_on_tour_entry = tk.Entry(root)
+send_on_tour_entry.pack()
+
+send_on_tour_button = tk.Button(root, text="Send On Tour", command=send_on_tour_button_clicked)
+send_on_tour_button.pack()
+
+return_from_tour_label = tk.Label(root, text="Return RTG from Tour:")
+return_from_tour_label.pack()
+
+return_from_tour_entry = tk.Entry(root)
+return_from_tour_entry.pack()
+
+return_from_tour_button = tk.Button(root, text="Return From Tour", command=return_from_tour_button_clicked)
+return_from_tour_button.pack()
+
+queue_label = tk.Label(root, text="")
+queue_label.pack()
+
+displayQueue()  # Initial display
+
+root.mainloop()
