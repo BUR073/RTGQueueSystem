bb8c4083ae2caa61e544b6441f525c5d38c818f5
BUR073 <93581465+BUR073@users.noreply.github.com>
2023-08-12 20:33:16 +0100

    Update RTGQueueSystem.py
    Full working version that include tables

--- RTGQueueSystem.py
+++ RTGQueueSystem.py
@@ -1,125 +1,106 @@
+# List to store RTGs currently on tour
 onTour = []
-tout = ["HEB", "ABC", "EFG", "GHI", "GHF", "IUY", "YRT", "NMB"]
-timeOut = ["YUT"]
 
+# Lists to manage RTGs in different states
+tout = ["HEB", "ABC", "EFG", "GHI", "GHF", "IUY", "YRT", "NMB"]  # RTGs available for tour
+timeOut = ["YUT"]  # RTGs in timeout
+
+# Function to add an RTG to appropriate list
 def addRTG(name):
-  if len(tout) < 8:
-    # Assuming number of touts allowed is 8 or les
-    # If there are 8 or more touts, new RTG will be put on timeout
-    tout.append(name)
-  else:
-    timeOut.append(name)
-
-def removeRTG(name):
-  if name in tout:
-    # Check if name is in tout list
-    tout.remove(name)
-  elif name in timeOut:
-    # Check if name is in time out list
-    timeOut.remove(name)
-  elif name in onTour:
-    # Else name must be in onTour list, you cannot remove someone from
-    # the roster while on tour
-    print("You cannot remove RTG from roster when on tour.")
-
-def sendOnTour(name):
-  if name in tout:
-    if name == tout[0]:
-      onTour.append([name])
-      # Add guid to onTour list
+    name = name.strip()  # Remove whitespace
+    if len(tout) < 8:
+        # Add RTG to tout if there are less than 8 RTGs on tour
+        tout.append(name)
     else:
-      # If the RTG going on tour is not the next person to go on tour
-      # then find out who they are behind and add it too a list
-      # along with the name of the RTG and add it to the onTour list
-      onTour.append([name, tout.index(name) + 1])
-      tout.remove(name)
-      # Remove guide from tout list
-      
-    if timeOut:
-      # If there are people in time out
-      tout.append(timeOut[0])
-      # Add the next person to the tout queue
-      del timeOut[0]
-      # And remove them from timeOut
-  else:
-    print("Name is not valid")
+        # Add RTG to timeout if tout is full
+        timeOut.append(name)
 
-def formatLists():
-  if len(tout) > 8:
-    diff = len(tout) - 8
-    for i in range(7, 7 + diff):
-      timeOut.insert(0, tout[i])
-      del tout[i]
-      
-      
+# Function to remove an RTG from appropriate list
+def removeRTG(name):
+    if name in tout:
+        tout.remove(name)  # Remove from tout list
+    elif name in timeOut:
+        timeOut.remove(name)  # Remove from timeout list
+    elif name in onTour:
+        print("You cannot remove RTG from roster when on tour.")  # Cannot remove while on tour
+
+# Function to send an RTG on tour
+def sendOnTour(name):
+    if name in tout:
+        if name == tout[0]:
+            onTour.append([name])  # Add RTG to onTour list
+            tout.remove(name)  # Remove from tout list
+            
+        if timeOut:
+            tout.append(timeOut[0])  # Add next person from timeout to tout queue
+            del timeOut[0]  # Remove from timeout
+    else:
+        print("Name is not valid")
+
+# Function to return an RTG from tour
 def returnFromTour(name):
-  if [name] in onTour:
-    onTour.remove(name)
-    addRTG(name)
-  else:
-    while True:
-      for i in tout:
-        if i[0] == name and i[1] in tout:
-          tout.insert(int(tout.index(i[1]) + 1), name)
-          formatLists()
-          break
-      for i in timeOut:
-          if i[0] == name and i[1] in timeOut:
-            timeOut.insert(int(timeOut.index(i[1]) + 1), name)
-            formatLists()
-            break
-        
-                    
-  # else:
-  #   print("RTG is not on tour")
+    name = name.strip()  # Trim whitespace
+    found = False  # Initialize a flag to track if the name is found
     
+    # Iterate through onTour list to find the RTG
+    for i in onTour:
+        if i[0] == name:
+            onTour.remove(i)  # Remove from onTour list
+            addRTG(name)  # Add back using addRTG function
+            found = True
+            break  # Exit loop once found
+    
+    if not found:
+        print(f"{name} is not on tour")
+
+# Function to display the current state of RTGs
+from tabulate import tabulate
+
 def displayQueue():
-  print("")
-  print("----------------------")
-  
-  print("Touting")
-  print("")
-  for num, i in enumerate(tout):
-    print(num+1, ":", i)
-  print("----------------------")
-  
-  print("Timeout")
-  print("")
-  for num, i in enumerate(timeOut):
-    print(num+1, ":", i)
-  print("----------------------") 
-  
-  print("On tour")
-  print("")
-  for num, i in enumerate(onTour):
-    print(num+1, ":", i[0])
-  print("----------------------")
-  
+    tout_table = [[num, rtg] for num, rtg in enumerate(tout, start=1)]
+    timeOut_table = [[num, rtg] for num, rtg in enumerate(timeOut, start=1)]
+    onTour_table = [[num, rtg[0]] for num, rtg in enumerate(onTour, start=1)]
+    
+    print("\nTouting Queue")
+    print(tabulate(tout_table, headers=["#", "RTG"]))
+    
+    print("\nTimeout Queue")
+    print(tabulate(timeOut_table, headers=["#", "RTG"]))
+    
+    print("\nOn Tour")
+    print(tabulate(onTour_table, headers=["#", "RTG"]))
+    print("----------------------\n")
+
+
+
+# Main program loop
 def main():
-  while True:
-    print("1. Add RTG")
-    print("2. Remove RTG")
-    print("3. Send RTG on tour")
-    print("4. RTG return from tour")
-    print("5. Display queue")
-    print("6. Exit")
-    x = int(input("Input: "))
-    print("")
-    if x == 6:
-      break
-    elif x == 1:
-      addRTG(input("RTG name: "))
-    elif x == 2:
-      removeRTG(input("RTG name: "))
-    elif x == 3:
-      sendOnTour(input("RTG name: "))
-    elif x == 4:
-      returnFromTour(input("RTG name: "))
-    elif x == 5:
-      displayQueue()
-    elif x == 6:
-      break
+    while True:
+        print("1. Add RTG")
+        print("2. Remove RTG")
+        print("3. Send RTG on tour")
+        print("4. RTG return from tour")
+        print("5. Display queue")
+        print("6. Exit")
+        x = int(input("Input: "))  # Get user choice
+        print("")
+        
+        # Execute the chosen action
+        if x == 6:
+            break
+        elif x == 1:
+            addRTG(input("RTG name: "))  # Add RTG
+        elif x == 2:
+            removeRTG(input("RTG name: "))  # Remove RTG
+        elif x == 3:
+            sendOnTour(input("RTG name: "))  # Send RTG on tour
+        elif x == 4:
+            returnFromTour(input("RTG name: "))  # Return RTG from tour
+        elif x == 5:
+            displayQueue()  # Display the queue
+        elif x == 6:
+            break
       
-main()
+main()  # Start the main program loop
     
   
